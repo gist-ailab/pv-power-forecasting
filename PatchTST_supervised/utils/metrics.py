fd49721f036825bbdcd31cb0aac667f029105cc2
psh9002 <psh9002@gmail.com>
2024-01-09 23:03:28 +0900

    ✨: add compute nRMSE function

--- PatchTST_supervised/utils/metrics.py
+++ PatchTST_supervised/utils/metrics.py
@@ -23,6 +23,9 @@ def MSE(pred, true):
 def RMSE(pred, true):
     return np.sqrt(MSE(pred, true))
 
+def nRMSE(pred, true, x_max, x_min):
+    return (np.sqrt(MSE(pred, true))/(x_max-x_min)) * 100
+
 
 def MAPE(pred, true):
     return np.mean(np.abs((pred - true) / true))
@@ -32,14 +35,15 @@ def MSPE(pred, true):
     return np.mean(np.square((pred - true) / true))
 
 
-def metric(pred, true):
+def metric(pred, true, x_max=None, x_min=None):
     mae = MAE(pred, true)
     mse = MSE(pred, true)
     rmse = RMSE(pred, true)
+    nrmse = nRMSE(pred, true, x_max, x_min)
     # mape = MAPE(pred, true)
     # mspe = MSPE(pred, true)
     # rse = RSE(pred, true)
     # corr = CORR(pred, true)
 
     # return mae, mse, rmse, mape, mspe, rse, corr
-    return mae, mse, rmse
+    return mae, mse, rmse, nrmse
