c69e59522cccc82a903fd1b9bb1456babc570a7f
sowonny <choiori97@gmail.com>
2024-04-09 07:55:44 +0900

    fix training code error

--- PatchTST_supervised/data_provider/data_loader.py
+++ PatchTST_supervised/data_provider/data_loader.py
@@ -606,7 +606,7 @@ class Dataset_pv_DKASC(Dataset):
     def inverse_transform(self, data):
         for i in range(5):
             data[:,:, i] = getattr(self, f'scaler_{self.domain}_{i}').inverse_transform(data[:,:, i])
-        return getattr(self, f'scaler_{self.domain}_4').inverse_transform(data)
+        return data
         # return self.scaler_DKASC_4.inverse_transform(data)
     
     
