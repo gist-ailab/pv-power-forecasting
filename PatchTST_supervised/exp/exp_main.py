47ff1b767329a727ac4e6886b84971e699f8b046
psh9002 <psh9002@gmail.com>
2024-05-02 05:14:32 +0900

    ✨: change MMDloss module for  CDTST and inverse transform

--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -4,13 +4,14 @@ from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, 
 from models.Stat_models import Naive_repeat, Arima
 from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop, visual_out, visual_original
 from utils.metrics import metric
-from utils.mmd_loss import MMDLoss
+# from utils.mmd_loss import MMDLoss
 
 import numpy as np
 import torch
 import torch.nn as nn
 from torch import optim
-from torch.optim import lr_scheduler 
+from torch.optim import lr_scheduler
+from pytorch_adapt.layers import MMDLoss, MMDBatchedLoss
 
 import os
 import time
@@ -57,9 +58,14 @@ class Exp_Main(Exp_Basic):
 
     def _select_criterion(self):
         criterion = nn.MSELoss()
-        cross_criterion = MMDLoss()
+        # cross_criterion = MMDLoss() # 안됨
+        # cross_criterion = MMDBatchedLoss(batch_size=self.args.batch_size, mmd_type='quadratic')
+        cross_criterion = MMDLoss(mmd_type="quadratic")
+        flatten = nn.Flatten()
+        # flatten은 [bs x nvars x d_model x patch_num]를 [bs x d_model x patch_num]을 nvars만큼 tensor를 갖는 list로 바꿔줄 때 사용
+        # https://github.com/KevinMusgrave/pytorch-adapt/blob/main/src/pytorch_adapt/layers/mmd_loss.py
         if self.args.model == 'PatchCDTST' and self.args.is_training:
-            return criterion, cross_criterion
+            return criterion, cross_criterion, flatten
         return criterion
 
     def train(self, setting, exp_id, resume):
@@ -88,7 +94,7 @@ class Exp_Main(Exp_Basic):
         model_optim = self._select_optimizer()
         
         if self.args.model == 'PatchCDTST':
-            criterion, cross_criterion = self._select_criterion()
+            criterion, cross_criterion, flatten = self._select_criterion()
         else:
             criterion = self._select_criterion()
             
@@ -189,16 +195,17 @@ class Exp_Main(Exp_Basic):
                     b_y_t = batch_y_t[:, -self.args.pred_len:, f_dim:].to(self.device)
                     loss_t = criterion(t_outputs, b_y_t)
                     
-                    # train_loss_t.append(loss_t.item())
-                    
-                    # loss for cross-domain
-                    # target_feat = target_feat[:, -self.args.pred_len:, f_dim:]
-                    # cross_feat = cross_feat[:, -self.args.pred_len:, f_dim:]
-                    # mmd_loss = cross_criterion(target_feat, cross_feat)
-                    target_f = target_feat[:, -self.args.pred_len:, f_dim:]
-                    cross_f = cross_feat[:, -self.args.pred_len:, f_dim:]
-                    mmd_loss = cross_criterion(target_f, cross_f)
-                    # train_mmd_loss.append(mmd_loss.item())
+                    # loss for cross-domain                    
+                    target_f = []
+                    cross_f = []
+                    # 차원 1은 nvars이고 이건 cross, target이 동일하여 동일 루프에서 사용
+                    for i in range(target_feat.size(1)):
+                        target_f_list = flatten(target_feat[:, i, :, :])
+                        cross_f_list = flatten(cross_feat[:, i, :, :])
+                        target_f.append(target_f_list)
+                        cross_f.append(cross_f_list)
+                    mmd_loss = cross_criterion(target_f, cross_f)   # list내에 각 변수 별로 tensor를 구성하여 MMD를 구하는 함수에 넣어 줌.
+
                     total_loss = loss_s + loss_t + mmd_loss
                 else:
                     total_loss = loss_s
@@ -234,13 +241,20 @@ class Exp_Main(Exp_Basic):
             
             train_losses = np.average(train_losses)
             if 'CDTST' in self.args.model:
-                vali_loss = self.vali(vali_data, vali_loader, criterion, cross_criterion)
-                test_loss = self.vali(test_data, test_loader, criterion, cross_criterion)
+                # vali_loss = self.vali(vali_data, vali_loader, criterion, cross_criterion, flatten)
+                # test_loss = self.vali(test_data, test_loader, criterion, cross_criterion, flatten)
+                val_total_loss, val_source_loss, val_target_loss = self.vali(vali_data, vali_loader, criterion, cross_criterion, flatten)
+                test_total_loss, test_source_loss, test_target_loss = self.vali(test_data, test_loader, criterion, cross_criterion, flatten)
+                print(f"Epoch: {epoch + 1} | Train Loss: {train_losses:.7f}")
+                print(f"Vali Source Domain Loss: {val_source_loss:.7f}, Vali Target Domain Loss: {val_target_loss:.7f}")
+                print(f"Test Source Domain Loss: {test_source_loss:.7f}, Test Target Domain Loss: {test_target_loss:.7f}")
+                print(f"\n")
+                vali_loss = val_target_loss
             else:
                 vali_loss = self.vali(vali_data, vali_loader, criterion)
                 test_loss = self.vali(test_data, test_loader, criterion)
+                print(f"Epoch: {epoch + 1} | Train Loss: {train_losses:.7f}, Vali Loss: {vali_loss:.7f}, Test Loss: {test_loss:.7f}")
             
-            print(f"Epoch: {epoch + 1} | Train Loss: {train_losses:.7f}, Vali Loss: {vali_loss:.7f}, Test Loss: {test_loss:.7f}")
             early_stopping(vali_loss, self.model, path)
             if early_stopping.early_stop:
                 print("Early stopping")
@@ -257,8 +271,11 @@ class Exp_Main(Exp_Basic):
 
         return self.model
 
-    def vali(self, vali_data, vali_loader, criterion, cross_criterion=None):
+    def vali(self, vali_data, vali_loader, criterion, cross_criterion=None, flatten=None):
         total_losses = []
+        total_source_losses = []
+        total_target_losses = []
+        
         self.model.eval()
         with torch.no_grad():
             for i, data in enumerate(vali_loader):
@@ -309,12 +326,12 @@ class Exp_Main(Exp_Basic):
                             source_outputs = self.model(batch_x_s, batch_x_mark_s, dec_inp, batch_y_mark_s)
                             
                 f_dim = -1 if self.args.features == 'MS' else 0
-                
-                # ### calculate metrics with only active power
+
                 source_outputs = source_outputs[:, -self.args.pred_len:, f_dim:].to(self.device)
                 batch_y_s = batch_y_s[:, -self.args.pred_len:, f_dim:].to(self.device)
+                                
                 if self.args.model != 'LSTM':
-
+                    ### calculate metrics with only active power
                     active_power_s = source_outputs[:, :, -1].detach().cpu()
                     active_power_gt_s = batch_y_s[:, :, -1].detach().cpu()
 
@@ -341,27 +358,48 @@ class Exp_Main(Exp_Basic):
                     active_power_gt_t = batch_y_t[:, :, -1].detach().cpu()
 
                     # de-normalize the data and prediction values for target domain
-                    active_power_np_t = vali_data.inverse_transform(active_power_t)
-                    active_power_gt_np_t = vali_data.inverse_transform(active_power_gt_t)
+                    active_power_np_t = vali_data.inverse_transform(active_power_t, domain='target')
+                    active_power_gt_np_t = vali_data.inverse_transform(active_power_gt_t, domain='target')
                     
                     pred_t = torch.from_numpy(active_power_np_t).to(self.device)
                     gt_t = torch.from_numpy(active_power_gt_np_t).to(self.device)
                     
                     loss_t = criterion(pred_t, gt_t)
                     
-                    # loss for cross-domain
-                    target_feat = target_feat[:, -self.args.pred_len:, f_dim:]
-                    cross_feat = cross_feat[:, -self.args.pred_len:, f_dim:]
-                    mmd_loss = cross_criterion(target_feat, cross_feat)
+                    # loss for cross-domain                    
+                    target_f = []
+                    cross_f = []
+                    # 차원 1은 nvars이고 이건 cross, target이 동일하여 동일 루프에서 사용
+                    for i in range(target_feat.size(1)):
+                        target_f_list = flatten(target_feat[:, i, :, :])
+                        cross_f_list = flatten(cross_feat[:, i, :, :])
+                        target_f.append(target_f_list)
+                        cross_f.append(cross_f_list)
+                    mmd_loss = cross_criterion(target_f, cross_f)   # list내에 각 변수 별로 tensor를 구성하여 MMD를 구하는 함수에 넣어 줌.
+                    
                     total_loss = loss_s + loss_t + mmd_loss
+                    
                 else:
                     total_loss = loss_s
-
-                total_losses.append(total_loss.item())
-        total_losses = np.average(total_losses)
+                    
+                
+                total_target_losses.append(loss_t.item())
+                if 'CDTST' in self.args.model:
+                    # train loop에서 early stopping을 위해 source, target loss를 따로 저장
+                    total_losses.append(total_loss.item())
+                    total_source_losses.append(loss_s.item())
+        
         self.model.train()
-        return total_losses
-
+        if 'CDTST' in self.args.model:
+            total_losses = np.average(total_losses)
+            total_source_losses = np.average(total_source_losses)
+            total_target_losses = np.average(total_target_losses)
+            # print(f"Vali Loss: {total_losses:.7f}, Source Loss: {total_source_losses:.7f}, Target Loss: {total_target_losses:.7f}")
+            # CDTST는 early stopping 적용을 target loss를 통해 확인하며, source loss, total loss는 확인용
+            return total_losses, total_source_losses, total_target_losses
+        else:
+            total_losses = np.average(total_losses)
+            return total_losses
 
 
     def test(self, setting, exp_id, model_path=None, test=0):
@@ -444,30 +482,19 @@ class Exp_Main(Exp_Basic):
 
                 f_dim = -1 if self.args.features == 'MS' else 0
                 # print(outputs.shape,batch_y.shape)
+
+                source_outputs = source_outputs[:, -self.args.pred_len:, f_dim:].to(self.device)
+                batch_y_s = batch_y_s[:, -self.args.pred_len:, f_dim:].to(self.device)
                 
-                # ### calculate metrics with only active power, BSH
                 # source domain
-                if self.args.model != 'LSTM':   
-
-                    # active_power = outputs[:, :, -1]
-                    # active_power_gt = batch_y[:, :, -1]
+                if self.args.model != 'LSTM':
+                    ### calculate metrics with only active power
+                    active_power_s = source_outputs[:, :, -1].detach().cpu()
+                    active_power_gt_s = batch_y_s[:, :, -1].detach().cpu()
                     
-                    active_power_s = source_outputs[:, :, f_dim]
-                    active_power_gt_s = batch_y_s[:, :, f_dim]
-
-                    
-                    active_power_np_s = active_power_s.detach().cpu().numpy()
-                    active_power_gt_np_s = active_power_gt_s.detach().cpu().numpy()
-                
-                    # active_power = outputs[:, :, -1].detach().cpu()
-                    # active_power_gt = batch_y[:, :, -1].detach().cpu()
-
-                    # active_power_np = active_power.detach().cpu().numpy()
-                    # active_power_gt_np = active_power_gt.detach().cpu().numpy()
-                    
-                # de-normalize the data and prediction values
-                    pred_s = test_data.inverse_transform(active_power_np_s)
-                    true_s = test_data.inverse_transform(active_power_gt_np_s)
+                    # de-normalize the data and prediction values
+                    pred_s = test_data.inverse_transform(active_power_s)
+                    true_s = test_data.inverse_transform(active_power_gt_s)
                
                 else:
                     pred_np = test_data.inverse_transform(source_outputs.detach().cpu().numpy())
@@ -499,24 +526,23 @@ class Exp_Main(Exp_Basic):
                         pd = np.concatenate((input_inverse_transform[0, :, -1], pred_s[0, :, -1]), axis=0)
                         visual_original(gt, pd, os.path.join(folder_path, str(i) + '.png'))
                 
-                if 'CDTST' in self.args.model:
+                if 'CDTST' in self.args.model and i % 10 == 0:
                     # target domain
-                    active_power_t = target_outputs[:, :, f_dim]
-                    active_power_gt_t = batch_y_t[:, :, f_dim]
-                    
-                    active_power_np_t = active_power_t.detach().cpu().numpy()
-                    active_power_gt_np_t = active_power_gt_t.detach().cpu().numpy()
+                    target_outputs = target_outputs[:, -self.args.pred_len:, f_dim:].to(self.device)
+                    batch_y_t = batch_y_t[:, -self.args.pred_len:, f_dim:].to(self.device)
+                    active_power_t = target_outputs[:, :, -1].detach().cpu()
+                    active_power_gt_t = batch_y_t[:, :, -1].detach().cpu()
                     
                     # de-normalize the data and prediction values
-                    pred_t = test_data.inverse_transform(active_power_np_t)
-                    true_t = test_data.inverse_transform(active_power_gt_np_t)
+                    pred_t = test_data.inverse_transform(active_power_t, domain='target')
+                    true_t = test_data.inverse_transform(active_power_gt_t, domain='target')
 
                     preds_t.append(pred_t)
-                    trues_t.append(true_t[:,-self.args.pred_len:])
+                    trues_t.append(true_t)
                     inputx_t.append(batch_x_t.detach().cpu().numpy())
                     if i % 10 == 0:                    
                         input_np_t = batch_x_t[:, :, -1].detach().cpu().numpy()
-                        input_inverse_transform_t = test_data.inverse_transform(input_np_t)
+                        input_inverse_transform_t = test_data.inverse_transform(input_np_t, domain='target')
                         input_seq_t = input_inverse_transform_t[0,:]
                         gt_t = true_t[0, -self.args.pred_len:]
                         pd_t = pred_t[0, :]
