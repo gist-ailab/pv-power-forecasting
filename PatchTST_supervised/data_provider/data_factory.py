8b820eee38def7180c459035851781c61f7ac94d
psh9002 <psh9002@gmail.com>
2024-10-03 00:36:31 +0900

    ♻️: change dataset name and delete `CDTST`

--- PatchTST_supervised/data_provider/data_factory.py
+++ PatchTST_supervised/data_provider/data_factory.py
@@ -1,18 +1,16 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Pred, \
-     Dataset_pv_DKASC, Dataset_pv_DKASC_multi,Dataset_pv_SolarDB, Dataset_pv_GIST, CrossDomain_Dataset
+     Dataset_DKASC_single, Dataset_DKASC, Dataset_GIST
 from torch.utils.data import DataLoader
 
 data_dict = {
+    'DKASC': Dataset_DKASC,
+    'DKASC_single': Dataset_DKASC_single,
+    'GIST': Dataset_GIST,
     'ETTh1': Dataset_ETT_hour,
     'ETTh2': Dataset_ETT_hour,
     'ETTm1': Dataset_ETT_minute,
     'ETTm2': Dataset_ETT_minute,
     'custom': Dataset_Custom,
-    'pv_DKASC': Dataset_pv_DKASC,
-    'pv_DKASC_multi': Dataset_pv_DKASC_multi,
-    'pv_SolarDB': Dataset_pv_SolarDB,
-    'pv_GIST': Dataset_pv_GIST,
-    'CrossDomain': CrossDomain_Dataset,
 }
 
 
@@ -38,32 +36,16 @@ def data_provider(args, flag):
         batch_size = args.batch_size
         freq = args.freq
 
-    if 'CDTST' in args.model:
-        data_set = Data(
-            source_root_path=args.source_root_path,
-            target_root_path=args.target_root_path,
-            flag=flag,
-            size=[args.seq_len, args.label_len, args.pred_len],
-            features=args.features,
-            source_data_path=args.source_data_path,
-            target_data_path=args.target_data_path,
-            target=args.target,
-            timeenc=timeenc,
-            freq=freq
-        )
-    else:
-        root_path = args.source_root_path
-        data_path = args.source_data_path
-        data_set = Data(
-            root_path=root_path,
-            data_path=data_path,
-            flag=flag,
-            size=[args.seq_len, args.label_len, args.pred_len],
-            features=args.features,
-            target=args.target,
-            timeenc=timeenc,
-            freq=freq
-        )
+    data_set = Data(
+        root_path=args.root_path,
+        data_path=args.data_path,
+        flag=flag,
+        size=[args.seq_len, args.label_len, args.pred_len],
+        features=args.features,
+        target=args.target,
+        timeenc=timeenc,
+        freq=freq
+    )
     print(flag, data_set.__len__())
     data_loader = DataLoader(
         data_set,
