35cf3234c20783686ab2013aa8851e08167c8fe4
sowonny <choiori97@gmail.com>
2024-10-24 11:42:14 +0900

    Fix: modify the config file

--- scripts/PatchTST/DKASC2GIST_fully_finetune.sh
+++ scripts/PatchTST/DKASC2GIST_fully_finetune.sh
@@ -20,22 +20,38 @@ random_seed=2024
 
 export CUDA_VISIBLE_DEVICES=3
 
-for pred_len in 24 #1 2 4 8 16
-do
+seq_len=(512 256 128 64)
+checkpoints=(
+    "/home/pv/code/PatchTST/checkpoints/24102211_PatchTST_DKASC_ftMS_sl512_ll0_pl16_dm128_nh16_el5_dl1_df1024_fc1_ebtimeF_dtTrue_Exp_0/checkpoint.pth"
+    "/home/pv/code/PatchTST/checkpoints/24102211_PatchTST_DKASC_ftMS_sl512_ll0_pl8_dm128_nh16_el5_dl1_df1024_fc1_ebtimeF_dtTrue_Exp_0/checkpoint.pth"
+    "/home/pv/code/PatchTST/checkpoints/24102211_PatchTST_DKASC_ftMS_sl512_ll0_pl4_dm128_nh16_el5_dl1_df1024_fc1_ebtimeF_dtTrue_Exp_0/checkpoint.pth"
+    "/home/pv/code/PatchTST/checkpoints/24102211_PatchTST_DKASC_ftMS_sl512_ll0_pl2_dm128_nh16_el5_dl1_df1024_fc1_ebtimeF_dtTrue_Exp_0/checkpoint.pth"
+    "/home/pv/code/PatchTST/checkpoints/24102211_PatchTST_DKASC_ftMS_sl512_ll0_pl1_dm128_nh16_el5_dl1_df1024_fc1_ebtimeF_dtTrue_Exp_0/checkpoint.pth"
+)
+
+for i in "${!seq_len[@]}"; do
+    sl=${seq_len[$i]}
+    ckpt=${checkpoints[$i]}
+
+
+    # echo "인덱스: $i, seq_len: $sl, checkpoint: $ckpt"
+
+    for pred_len in 16 8 4 2 1 
+    do
     python -u run_finetune.py \
       --gpu 0 \
       --use_amp \
       --random_seed $random_seed \
       --is_training 1 \
       --is_fully_finetune 1 \
-      --checkpoints  /SSDe/sowon_choi/PatchTST/checkpoints/24102105_PatchTST_DKASC_ftMS_sl336_ll0_pl24_dm128_nh16_el5_dl1_df1024_fc1_ebtimeF_dtTrue_Exp_0/checkpoint.pth \
+      --checkpoints "$ckpt" \
       --root_path $root_path_name \
       --data_path $data_path_name \
       --model_id $model_id \
       --model $model_name \
       --data $data_name \
       --features MS \
-      --seq_len $seq_len \
+      --seq_len "$sl" \
       --label_len $label_len \
       --pred_len $pred_len \
       --enc_in 5 \
@@ -53,4 +69,5 @@ do
       --patience 20\
       --embed 'timeF' \
       --itr 1 --batch_size 512 --learning_rate 0.0001 >logs/$exp_id/$model_name'_'$data_name'_'$seq_len'_'$pred_len.log
+    done
 done
--- scripts/PatchTST/DKASC_pretrain_2.sh
+++ scripts/PatchTST/DKASC_pretrain_2.sh
@@ -12,16 +12,19 @@ fi
 seq_len=336
 label_len=0
 
-root_path_name=/ailab_mat/dataset/PV/DKASC_AliceSprings/preprocessed
+root_path_name=/PV/DKASC_AliceSprings/preprocessed
 data_path_name='type=all'
 data_name=DKASC
 random_seed=2024
 
 
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=0
 
-for pred_len in 24 #1 2 4 8 16
+#for pred_len in 1 2 4 8 16
+for seq_len in 512 256 128 64
 do
+    for pred_len in 16 8 4 2 1 
+    do
     python -u run_longExp.py \
       --gpu 0 \
       --use_amp \
@@ -36,10 +39,10 @@ do
       --seq_len $seq_len \
       --label_len $label_len \
       --pred_len $pred_len \
-      --enc_in 10 \
-      --e_layers 10 \
-      --n_heads 32 \
-      --d_model 384 \
+      --enc_in 5 \
+      --e_layers 5 \
+      --n_heads 16 \
+      --d_model 128 \
       --d_ff 1024 \
       --dropout 0.05\
       --fc_dropout 0.05\
@@ -51,4 +54,5 @@ do
       --patience 20\
       --embed 'timeF' \
       --itr 1 --batch_size 512 --learning_rate 0.0001 >logs/$exp_id/$model_name'_'$data_name'_'$seq_len'_'$pred_len.log
+    done
 done
