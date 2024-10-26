1841151caf94776d6f7094cad1ebe2dd4b487545
sowonny <choiori97@gmail.com>
2024-10-26 18:20:49 +0900

    Fix: pretrain configs

--- scripts/PatchTST/DKASC_pretrain.sh
+++ scripts/PatchTST/DKASC_pretrain.sh
@@ -13,19 +13,20 @@ seq_len=256
 label_len=0
 
 root_path_name=/ailab_mat/dataset/PV/DKASC_AliceSprings/converted
+data_path_name='type=all'
 data_name=DKASC
 random_seed=2024
 
 
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=4
 
-for pred_len in 24 #1 2 4 8 16
+for pred_len in 16 8 4 2 1
 do
     python -u run_longExp.py \
       --gpu 0 \
       --use_amp \
       --random_seed $random_seed \
-      --is_training 1 \
+      --is_pretraining 1 \
       --root_path $root_path_name \
       --data_path $data_path_name \
       --model_id $model_id \
@@ -35,11 +36,11 @@ do
       --seq_len $seq_len \
       --label_len $label_len \
       --pred_len $pred_len \
-      --enc_in 5 \
-      --e_layers 5 \
-      --n_heads 16 \
-      --d_model 128 \
-      --d_ff 1024 \
+      --enc_in 7 \
+      --e_layers 10 \
+      --n_heads 8 \
+      --d_model 1024 \
+      --d_ff 4096 \
       --dropout 0.05\
       --fc_dropout 0.05\
       --head_dropout 0\
@@ -49,5 +50,5 @@ do
       --train_epochs 100\
       --patience 20\
       --embed 'timeF' \
-      --itr 1 --batch_size 512 --learning_rate 0.0001 >logs/$exp_id/$model_name'_'$data_name'_'$seq_len'_'$pred_len.log
+      --itr 1 --batch_size 256 --learning_rate 0.0001 >logs/$exp_id/$model_name'_'$data_name'_'$seq_len'_'$pred_len.log
 done
--- scripts/PatchTST/DKASC_pretrain_2.sh
+++ scripts/PatchTST/DKASC_pretrain_2.sh
@@ -9,32 +9,25 @@ if [ ! -d "./logs/$exp_id" ]; then
     mkdir -p ./logs/$exp_id
 fi
 
-seq_len=336
-label_len=0
-
-root_path_name=/PV/DKASC_AliceSprings/converted
-data_path_name='type=all'
-data_name=DKASC
-random_seed=2024
-
 
 seq_len=256
 label_len=0
 
 root_path_name=/ailab_mat/dataset/PV/DKASC_AliceSprings/converted
+data_path_name='type=all'
 data_name=DKASC
 random_seed=2024
 
 
 export CUDA_VISIBLE_DEVICES=5
 
-for pred_len in 24 #1 2 4 8 16
+for pred_len in 16 8 4 2 1
 do
     python -u run_longExp.py \
       --gpu 0 \
       --use_amp \
       --random_seed $random_seed \
-      --is_training 1 \
+      --is_pretraining 1 \
       --root_path $root_path_name \
       --data_path $data_path_name \
       --model_id $model_id \
@@ -44,11 +37,11 @@ do
       --seq_len $seq_len \
       --label_len $label_len \
       --pred_len $pred_len \
-      --enc_in 5 \
+      --enc_in 7 \
       --e_layers 8 \
-      --n_heads 16 \
-      --d_model 128 \
-      --d_ff 1024 \
+      --n_heads 8 \
+      --d_model 512 \
+      --d_ff 2048 \
       --dropout 0.05\
       --fc_dropout 0.05\
       --head_dropout 0\
@@ -58,5 +51,5 @@ do
       --train_epochs 100\
       --patience 20\
       --embed 'timeF' \
-      --itr 1 --batch_size 512 --learning_rate 0.0001 >logs/$exp_id/$model_name'_'$data_name'_'$seq_len'_'$pred_len.log
+      --itr 1 --batch_size 256 --learning_rate 0.0001 >logs/$exp_id/$model_name'_'$data_name'_'$seq_len'_'$pred_len.log
 done
