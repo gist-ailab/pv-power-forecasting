47586ff69ccade8349b2141c6c771160f2c48bfc
sowonny <choiori97@gmail.com>
2024-11-03 16:38:23 +0900

    Fix: OEDI CA dataloder, config enc_in

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -2327,174 +2327,191 @@ class Dataset_OEDI_California(Dataset):
         ]
         
         self.scalers = {}
-        self.x_list = []
-        self.y_list = []
-        self.ds_list = []
+        self.site_data = {}
 
-       
-        random.seed(42)
-        if self.data_path['type'] == 'all':
-            print("all")
-            random.shuffle(self.LOCATIONS)  # 데이터 섞기
-            total_size = len(self.LOCATIONS)
-            train_size = int(0.6 * total_size)
-            val_size = int(0.3 * total_size)
-                    
-            if self.flag == 'train':
-                self.train = self.LOCATIONS[:train_size]
-                print(f'[INFO] Train Locations: total {len(self.train)}')
-                print(f'[INFO] Location List: {self.train}')
-                
-            elif self.flag == 'val':
-                self.val = self.LOCATIONS[train_size:train_size + val_size]
-                print(f'[INFO] Valid Locations: total {len(self.val)}')
-                print(f'[INFO] Location List: {self.val}')
+        self.load_preprocessed_data()
+        self.indices = self.create_sequences_indices()
 
-            elif self.flag == 'test':
-                self.test = self.LOCATIONS[train_size + val_size:]
-                print(f'[INFO] Test Locations: total {len(self.test)}')
-                print(f'[INFO] Location List: {self.test}')
-
-
-
-        elif self.data_path['type'] == 'debug':
-            
-            if self.flag == 'train':
-                self.train = self.data_path['train']
-            
-            elif self.flag == 'val':
-                self.val = self.data_path['val']
-            
-            elif self.flag == 'test':
-                self.test = self.data_path['test']
-            
-
-            
         
-        self.load_preprocessed_data() 
-       
-
-
+    
     def load_preprocessed_data(self):
+        # 파일 로드 및 사이트별 데이터 수집
+        self._load_files()
+        # train, val, test split, 해당하는 flag에 맞게 데이터 가져옴
+        selected_sites = self._split_sites()[self.flag]
+        site_data = {}
+        all_data = [] # 전체 데이터셋
 
-       
-        # 1. Flag에 맞게 데이터 불러오기
-        if self.flag == 'train':
-            df_raw = self.load_and_concat_data(self.train)
+        for site_id in selected_sites:
+            site_files = self.site_files.get(site_id, [])
+            for file_name in site_files:
+                file_path = os.path.join(self.root_path, file_name)
+                df_raw = pd.read_csv(file_path)
+                df_x, df_y, time_feature, timestamp, site = self._process_file(df_raw, site_id)
+
+                # 데이터 저장
+                site_data.setdefault(site_id, []).append({
+                    'x': df_x,
+                    'y': df_y,
+                    'data_stamp': time_feature,
+                    'timestamp': timestamp
+                })
+
+                # for scaling
+                all_data.append(df_x)
+
         
-        elif self.flag == 'val':
-            df_raw = self.load_and_concat_data(self.val)
+            combined_data = pd.concat(all_data, axis=0)
+           
+            # 스케일러 적용        
+            if self.scaler:
+                # train때는 전체 데이터셋에 대해 칼럼 별로 스케일러를 fit
+                if self.flag == 'train':
+                    for col in combined_data.columns:
+                        if col == 'timestamp': continue
+                        if self.scaler == 'MinMaxScaler':
+                            scaler = MinMaxScaler()
+                        else: 
+                            scaler = StandardScaler()
+                        
+                        scaler.fit(combined_data[[col]])
+                        self.scalers[col] = scaler
+                        with open(os.path.join(self.root_path, f'{col}_scaler.pkl'), 'wb') as f:
+                                    pickle.dump(scaler, f)
+                else:
+                    for col in combined_data.columns:
+                        if col == 'timestamp': continue
+                        with open(os.path.join(self.root_path, f'{col}_scaler.pkl'), 'rb') as f:
+                            scaler = pickle.load(f)
+                            self.scalers[col] = scaler
 
-        elif self.flag == 'test':
-            df_raw = self.load_and_concat_data(self.test)
+                # scaler 적용 및 site data update
+                for site_id in site_data.keys():
+                    for data in site_data[site_id]:
+                        for col in data['x'].columns:
+                            if col == 'timestamp': continue
+                            scaler = self.scalers[col]
+                            data['x'][col] = scaler.transform(data['x'][[col]])  
+                            if col == 'Active_Power':    
+                                data['y'][col] = scaler.transform(data['y'][[col]])
+        
+            self.site_data = site_data
 
-        # 칼럼 순서와 이름 확인
-        print(f"[INFO] Loaded columns: {df_raw.columns}")
+    def _process_file(self, df_raw, site_id):
+        # Time encoding ((년,) 월, 일, 요일, 시간)
 
-        # 2. Time encoding (년, 월, 일, 요일, 시간)
+        data_stamp = pd.DataFrame()
+        data_stamp['date'] = pd.to_datetime(df_raw.timestamp)
+        data_stamp['year'] = data_stamp.date.apply(lambda row: row.year, 1)
+        data_stamp['month'] = data_stamp.date.apply(lambda row: row.month, 1)
+        data_stamp['day'] = data_stamp.date.apply(lambda row: row.day, 1)
+        data_stamp['weekday'] = data_stamp.date.apply(lambda row: row.weekday(), 1)
+        data_stamp['hour'] = data_stamp.date.apply(lambda row: row.hour, 1)
         if self.timeenc == 0:
-            data_stamp = pd.DataFrame()
-            data_stamp['date'] = pd.to_datetime(df_raw.timestamp)
-            data_stamp['year'] = df_raw.date.apply(lambda row: row.year, 1)
-            data_stamp['month'] = df_raw.timestamp.apply(lambda row: row.month, 1)
-            data_stamp['day'] = df_raw.date.apply(lambda row: row.day, 1)
-            data_stamp['weekday'] = df_raw.date.apply(lambda row: row.weekday(), 1)
-            data_stamp['hour'] = df_raw.date.apply(lambda row: row.hour, 1)
-            
-            data_stamp = data_stamp.drop(['date'], axis=1).values()
-
-        elif self.timeenc == 1:
-            data_stamp = time_features(pd.to_datetime(df_raw['timestamp'].values), freq=self.freq)
-            data_stamp = data_stamp.transpose(1, 0)
-        
-        # 3. Encoding 후, date columns 생성
-        # df_raw['date'] = data_stamp
+            time_feature = data_stamp[['month', 'day', 'weekday', 'hour']].values
+        else: 
+            time_feature = time_features(pd.to_datetime(df_raw['timestamp'].values), freq=self.freq).T
+        timestamp = data_stamp[['year', 'month', 'day', 'weekday', 'hour']].values
 
 
-               
-        # 4. Columns 순서 정렬 (timestamp, date, ..... , target)
+        # 필요한 칼럼만 추출
         cols = df_raw.columns.tolist()
-        # cols.remove('date')
+        print(cols)
         cols.remove('timestamp')
         cols.remove('Active_Power')
-        # cols.remove('Wind_Speed')
+        
+        # df_raw = df_raw[['date'] + cols + [self.target]]
+    
         df_raw = df_raw[cols + [self.target]]
 
-        if self.scale: 
-            # Train일 때는, Scaler Fit 후에, 저장
-            if self.flag == 'train' or self.flag == 'val' or self.flag == 'test':
-                for col in df_raw.columns:
-                    scaler = StandardScaler()
-                    df_raw[col] = scaler.fit_transform(df_raw[[col]])
-                    self.scalers[col] = scaler 
-                    # Scaler를 pickle 파일로 저장
-                    path = os.path.join(self.root_path, f'{col}_scaler.pkl')
-                    with open(path, 'wb') as f:
-                        pickle.dump(scaler, f)
-        
-            else:
-                # Val, Test일 때는, 저장된 Scaler 불러와서 적용
-                self.scalers = {}
-                transformed_df = df_raw.copy()  
+        # TODO: MS, S, M 구분해서 처리하는 코드 추가
 
-                for col in df_raw.columns:
-                    path = os.path.join(self.root_path, f'{col}_scaler.pkl')
-            
-                    # Scaler가 존재하는지 확인
-                    if os.path.exists(path):
-                        with open(path, 'rb') as f:
-                            scaler = pickle.load(f) 
-                
-                        # 해당 칼럼에 스케일러 적용 (transform)
-                        df_raw[col] = scaler.transform(transformed_df[[col]]) 
-                        self.scalers[col] = scaler
-                
-                    else:
-                        print(f"Scaler for column {col} not found.")
-
-
-        # 6. 입력할 칼럼들 지정하여 리스트 생성
-        if self.features == 'M' or self.features == 'MS':
-            # date 열을 제외
-            cols_data = df_raw.columns[1:]
-            df_x = df_raw[cols_data]
-        elif self.features == 'S':
-            # Active Power만 추출
-            df_x = df_raw[[self.target]]
+        df_x = df_raw[cols + [self.target]]
+        # df_x['Wind_Speed'] = np.log
+        df_y = df_raw[[self.target]]
+        site_id = np.array([site_id]) * len(df_x)
        
-  
-        self.x_list = df_x.values
-        # 타겟은 마지막 열인 Active_Power
-        self.y_list = df_raw[[self.target]].values
-        # date columns만 선택
-        self.ds_list = data_stamp      
-        
-    # 파일 경로를 가져와서 DataFrame으로 합치는 함수
-    def load_and_concat_data(self, file_list):
-        df_list = []
-        for file in file_list:
-            file_path = f"{self.root_path}/{file}"  
-            df = pd.read_csv(file_path)  
-            assert (df.isnull().sum()).sum() == 0, "허용되지 않은 열에 결측치가 존재합니다."            
-            df_list.append(df) 
-        return pd.concat(df_list, ignore_index=True) 
+        return df_x, df_y, time_feature, timestamp, site_id
 
+
+    def _load_files(self):
+        if self.data_path['type'] == 'all':
+            all_files = [f for f in os.listdir(self.root_path) if f.endswith('.csv')]
+            self.site_files = {}
+            for file_name in all_files:
+                site_id = int(file_name.split('.')[0].split('inv')[-1])
+                self.site_files.setdefault(site_id, []).append(file_name)
+        if self.data_path['type'] == 'debug':
+            self.site_files = {}
+            for key in ['train', 'val', 'test']:
+                site_id = int(self.data_path[key].split('-')[0])
+                self.site_files.setdefault(site_id, []).append(self.data_path[key])
+    
+    
+    def _split_sites(self):
+        if self.data_path['type'] == 'all':
+            site_ids = list(self.site_files.keys())
+            
+            # TODO: 재현 가능하게 고정하기
+            # [INFO] Train sites: [92, 214, 52, 61, 99, 60, 72, 68, 55, 67, 64, 212, 59, 71, 69, 66, 90, 73, 70, 79, 213, 74, 85]
+            # train 1310017
+            # [INFO] Val sites: [54, 58, 63, 98, 77, 84]
+            # val 348608
+            # [INFO] Test sites: [57, 93, 56, 218, 100]
+            # test 250599
+
+            random.seed(42)
+            random.shuffle(site_ids)
+            total_sites = len(site_ids)
+            train_end = int(total_sites * 0.7)
+            val_end = train_end + int(total_sites * 0.2)
+
+            site_split = {
+                'train': site_ids[:train_end],
+                'val': site_ids[train_end:val_end],
+                'test': site_ids[val_end:]  
+            }
+        elif self.data_path['type'] == 'debug':
+            site_split = {
+                'train': [int(self.data_path['train'].split('-')[0])],
+                'val': [int(self.data_path['val'].split('-')[0])],
+                'test': [int(self.data_path['test'].split('-')[0])]
+            }
+
+        print(f'[INFO] {self.flag.capitalize()} sites: {site_split[self.flag]}')
+        return site_split
+    
+    def create_sequences_indices(self):
+        indices = []
+        for site_id, data_list in self.site_data.items():
+            for data in data_list:
+                x_len = len(data['x'])
+                max_start = x_len - self.seq_len - self.pred_len + 1
+                indices.extend([(site_id, data, i) for i in range(max_start)])
+        return indices
+
+    
     def __getitem__(self, index):
-        s_begin = index
+        site_id, data, start_idx = self.indices[index]
+        s_begin = start_idx
         s_end = s_begin + self.seq_len
         r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
-        
-        seq_x = self.x_list[s_begin:s_end]
-        seq_y = self.y_list[r_begin:r_end]
-        seq_x_mark = self.ds_list[s_begin:s_end]
-        seq_y_mark = self.ds_list[r_begin:r_end]
 
-        return seq_x, seq_y.reshape(-1, 1), seq_x_mark, seq_y_mark
+        seq_x = data['x'].iloc[s_begin:s_end].values
+        seq_y = data['y'].iloc[r_begin:r_end].values
+        seq_x_mark = data['data_stamp'][s_begin:s_end]
+        seq_y_mark = data['data_stamp'][r_begin:r_end]
+        site = np.array([site_id]).repeat(s_end - s_begin).reshape(-1, 1)
+        seq_x_ds = data['timestamp'][s_begin:s_end]
+        seq_y_ds = data['timestamp'][r_begin:r_end]
 
+        return seq_x, seq_y.reshape(-1, 1), seq_x_mark, seq_y_mark, site, seq_x_ds, seq_y_ds
+    
+    
+    def __len__(self):    
+        return len(self.indices)
 
-    def __len__(self):
-        return len(self.x_list) - self.seq_len - self.pred_len + 1
 
 
     # 평가 시 필요함
@@ -2504,7 +2521,7 @@ class Dataset_OEDI_California(Dataset):
         data[-1] = self.scalers['Active_Power'].inverse_transform(data[-1].reshape(-1, 1))
         data = data.reshape(data_org.shape[0], data_org.shape[1], -1)
         return data
-
+    
 class Dataset_Miryang(Dataset):
     def __init__(self, root_path, flag='train', size=None,
                 features='MS', data_path='', target='Active_Power',
