dfad979ea00b80ada814ede85e63d9c5cf59ced2
sowonny <choiori97@gmail.com>
2024-12-22 02:45:31 +0900

    ✨: add new script for DKASC pretraining and modify existing script for improved configuration

--- scripts/PatchTST/DKASC_pl24_ll0_nh8_el6_dm256_df1024_patch24 copy.sh
+++ scripts/PatchTST/DKASC_pl24_ll0_nh8_el6_dm256_df1024_patch24 copy.sh
@@ -13,7 +13,7 @@ root_path_name="/ailab_mat/dataset/PV/DKASC/processed_data_all/"
 data_name=DKASC
 random_seed=2024
 
-seq_len=240
+# seq_len=240
 pred_len=24
 label_len=0
 
@@ -23,12 +23,11 @@ d_model=256
 d_ff=1024
 patch_len=24
 
-local_rank=0
-export CUDA_VISIBLE_DEVICES=0,1
-export WORLD_SIZE=2 # 총 프로세스 수
-export MASTER_ADDR='localhost'
-# export MASTER_PORT='12356'  # 임의의 빈 포트
-export MASTER_PORT=12356  # 임의의 빈 포트
+export CUDA_VISIBLE_DEVICES=0
+# export WORLD_SIZE=2 # 총 프로세스 수
+# export MASTER_ADDR='localhost'
+# # export MASTER_PORT='12356'  # 임의의 빈 포트
+# export MASTER_PORT=12356  # 임의의 빈 포트
 
 # CPU 코어 개수와 num_workers 계산
 total_cores=$(nproc)  # 전체 CPU 코어 개수
@@ -37,25 +36,17 @@ if [ "$num_workers" -lt 1 ]; then
     num_workers=1
 fi
 
-setting_name="${model_name}_sl${seq_len}_pl${pred_len}_ll${label_len}_nh${n_heads}_el${e_layers}_dm${d_model}_df${d_ff}_patch${patch_len}"
-
 echo "Total CPU cores: $total_cores"
 echo "Using num_workers: $num_workers"
-echo "Generated setting name: $setting_name"
 
-# 환경 변수 출력
-echo "Environment variables before torchrun:"
-echo "LOCAL_RANK: $LOCAL_RANK"
-echo "RANK: $RANK"
-echo "WORLD_SIZE: $WORLD_SIZE"
-echo "MASTER_ADDR: $MASTER_ADDR"
-echo "MASTER_PORT: $MASTER_PORT"
-
-for pred_len in 24
+for seq_len in 168 240 336
 do
-    torchrun --nproc_per_node=$WORLD_SIZE --master_port=$MASTER_PORT run_longExp.py \
+    setting_name="${model_name}_sl${seq_len}_pl${pred_len}_ll${label_len}_nh${n_heads}_el${e_layers}_dm${d_model}_df${d_ff}_patch${patch_len}"
+    echo "Generated setting name: $setting_name"
+    python run_longExp.py \
         --random_seed $random_seed \
         --is_pretraining 1 \
+        --run_name $setting_name \
         --model_id $model_id \
         --model $model_name \
         --data $data_name \
@@ -79,6 +70,8 @@ do
         --batch_size 256 \
         --learning_rate 0.0001 \
         --gpu 0 \
+        --local_rank 0 \
         --des 'Exp' \
-        --distributed >logs/$exp_id/$model_name'_'$data_name'_'$seq_len'_'$pred_len'_'$n_heads'_'$patch_len.log
+        --distributed \
+        --wandb >logs/$exp_id/$model_name'_'$data_name'_'$seq_len'_'$pred_len'_'$n_heads'_'$patch_len.log
 done
