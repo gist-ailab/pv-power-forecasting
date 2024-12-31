684ae7de4312894d741cad4bc3bfa08531c32b6c
psh9002 <psh9002@gmail.com>
2024-12-31 18:05:24 +0000

    ✨: add num_freeze_layers variable and update setting name in DKASC_to_GIST script for clarity

--- scripts/PatchTST/DKASC_to_GIST_nh16_el8_dm512_df2408_patch24.sh
+++ scripts/PatchTST/DKASC_to_GIST_nh16_el8_dm512_df2408_patch24.sh
@@ -15,6 +15,8 @@ fi
 root_path_name="/ailab_mat/dataset/PV/${data_name}/processed_data_${data_type}/"
 random_seed=2024
 
+num_freeze_layers=1
+
 seq_len=240
 pred_len=24
 label_len=0
@@ -25,7 +27,7 @@ d_model=512
 d_ff=2048
 patch_len=24
 
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=0
 # export WORLD_SIZE=2 # 총 프로세스 수
 # export MASTER_ADDR='localhost'
 # export MASTER_PORT=12356  # 임의의 빈 포트
@@ -41,13 +43,13 @@ echo "Total CPU cores: $total_cores"
 echo "Using num_workers: $num_workers"
 echo "Using CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 
-
-setting_name="DKASC_to_GIST_linear_probe_${data_type}_${model_name}_sl${seq_len}_pl${pred_len}_ll${label_len}_nh${n_heads}_el${e_layers}_dm${d_model}_df${d_ff}_patch${patch_len}"
+# setting_name="DKASC_to_GIST_linear_probe_${data_type}_${model_name}_sl${seq_len}_pl${pred_len}_ll${label_len}_nh${n_heads}_el${e_layers}_dm${d_model}_df${d_ff}_patch${patch_len}"
+setting_name="DKASC_to_GIST_freeze_${num_freeze_layers}_${data_type}_${model_name}_sl${seq_len}_pl${pred_len}_ll${label_len}_nh${n_heads}_el${e_layers}_dm${d_model}_df${d_ff}_patch${patch_len}"
 echo "Generated setting name: $setting_name"
 python run_longExp.py \
     --run_name $setting_name \
     --random_seed $random_seed \
-    --linear_probe \
+    --num_freeze_layers $num_freeze_layers \
     --model_id $model_id \
     --model $model_name \
     --data $data_name \
