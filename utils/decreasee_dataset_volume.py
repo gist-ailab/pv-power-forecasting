7128a381f9a892f20e1b09a8cafea8b6ac9d3f41
psh9002 <psh9002@gmail.com>
2025-01-02 04:19:45 +0900

    ✨: update dataset name and directory structure; adjust reduction factor and improve output path handling

--- utils/decreasee_dataset_volume.py
+++ utils/decreasee_dataset_volume.py
@@ -5,9 +5,9 @@ from tqdm import tqdm
 
 
 # Directory containing the CSV files
-dataset_name = 'Source' # DKASC_AliceSprings, DKASC_Yulara, Germany, GIST, Miryang, OEDI_California, OEDI_Georgia, UK
+dataset_name = 'DKASC' # DKASC_AliceSprings, DKASC_Yulara, Germany, GIST, Miryang, OEDI_California, OEDI_Georgia, UK
 
-directory = f'/ailab_mat/dataset/PV/{dataset_name}/processed_data_day/'
+directory = f'/ailab_mat/dataset/PV/{dataset_name}/processed_data_all/'
 
 # Find all files ending with '.csv'
 csv_files = glob(os.path.join(directory, '*.csv'))
@@ -16,7 +16,7 @@ csv_files = glob(os.path.join(directory, '*.csv'))
 processed_data = []
 
 # Set the reduction factor (e.g., 1/8 or 1/16)
-reduction_factor = 16  # Change to 16 for 1/16
+reduction_factor = 8  # Change to 16 for 1/16
 
 # Loop through each CSV file
 for file in tqdm(csv_files, desc=f'Reduction divided by {reduction_factor} processing'):
@@ -36,11 +36,9 @@ for file in tqdm(csv_files, desc=f'Reduction divided by {reduction_factor} proce
     processed_data.append(reduced_data)
     
     # Optional: Save the reduced file
-    output_path = file.replace('processed_data_day', f'processed_data_day_reduced_{reduction_factor}')
+    output_path = file.replace('processed_data_all', f'processed_data_all_reduced_{reduction_factor}')
     output_dir = os.path.dirname(output_path)
 
-    # 디렉토리가 없으면 생성
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
 
     reduced_data.to_csv(output_path, index=False)
